e33adba29f2ce212e69ddefd94bca7671b2fbbab
lai2-24 <lai2-24@itismeucci.com>
2024-10-10 13:49:16 +0200

    modifica input

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -6,18 +6,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+import java.util.Scanner;
+//client
 public class Main {
     
     public static void main(String[] args) throws UnknownHostException, IOException {
+        Scanner scanner = new Scanner(System.in);
+         
+    Socket client= new Socket("localhost",3000);
+    BufferedReader in= new BufferedReader(new InputStreamReader(client.getInputStream()));
+    DataOutputStream out=new DataOutputStream(client.getOutputStream());
+    String stringaMinuscola;
+    do{
+
         System.out.println("client partito");
-        Socket client= new Socket("localhost",3000);
-        BufferedReader in= new BufferedReader(new InputStreamReader(client.getInputStream()));
-        DataOutputStream out=new DataOutputStream(client.getOutputStream());
-        String stringaMinuscola="ciao";
+        stringaMinuscola = scanner.nextLine(); 
         out.writeBytes(stringaMinuscola+'\n');
+        
         String stringaRicevuta=in.readLine();
-        System.out.println("stringa minuscola "+stringaRicevuta);
+        System.out.println(stringaRicevuta);
+        
+    }while(!stringaMinuscola.equals("!"));
         System.out.println("client terminato");
     }
 }
--- server/src/main/java/com/example/Main.java
+++ server/src/main/java/com/example/Main.java
@@ -7,20 +7,37 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("server partito...");
-        ServerSocket server= new ServerSocket(3000);
-        Socket miosocket=server.accept();
-        BufferedReader in= new BufferedReader(new InputStreamReader(miosocket.getInputStream()));
-        DataOutputStream out=new DataOutputStream(miosocket.getOutputStream());
-        String stringaRicevuta=in.readLine();
-        System.out.println(stringaRicevuta+"questa e' la stringa ricevuta");
-        String stringaMaiuscola=stringaRicevuta.toUpperCase();
-        out.writeBytes(stringaMaiuscola);
-        miosocket.close();
-        server.close();
-        System.out.println("ciao a tutti");
-        
+       
+
+           
+           ServerSocket server= new ServerSocket(3000);
+           Socket miosocket=server.accept();
+           
+           BufferedReader in= new BufferedReader(new InputStreamReader(miosocket.getInputStream()));
+           DataOutputStream out=new DataOutputStream(miosocket.getOutputStream());
+           System.out.println("server partito...");
+           String stringaRicevuta;
+           do{
+
+               stringaRicevuta=in.readLine();
+               if(stringaRicevuta.equals("!"))
+               {
+                miosocket.close();
+                server.close();
+                System.out.println("server terminato");
+               }
+               System.out.println(stringaRicevuta);
+               String stringaMaiuscola=stringaRicevuta.toUpperCase();
+               out.writeBytes(stringaMaiuscola);
+            }while(!stringaRicevuta.equals("!"));
+           
+           
+          
     }
+        
+        
+        
 }
